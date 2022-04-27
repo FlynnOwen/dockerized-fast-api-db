d74e328a6178c234810fb2f390f6fe5ae96ac86c
Flynn Owen <flynnoo49@gmail.com>
2022-04-27 21:28:01 +1200

    add basic endpoints

---
+++ app/app.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get('/', tags=["root"] )
+async def read_root() -> dict:
+    return {"message": "Welcome to the FastAPI Dockerized app!"}
+
+
+@app.get('/human/{human_id}', tags=["human"] )
+async def get_human(human_id) -> dict:
+    return {"message": "This is your human"}
+
+
+@app.post('/human/{human_id}', tags=["human"] )
+async def make_human(human_id) -> dict:
+    return {"message": "This is your human"}
