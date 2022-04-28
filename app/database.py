b18849dcf3918ea3e3866167d593ba55f57b8f5a
Flynn Owen <flynnoo49@gmail.com>
2022-04-28 14:16:18 +1200

    change to use postgres db

--- app/database.py
+++ app/database.py
@@ -1,9 +1,16 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
 
-DATABASE_URL = "sqlite:///./test.db"
+#DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = f"postgresql://{os.getenv(POSTGRES_USER)}"\
+               f":{os.getenv(POSTGRES_PASSWORD)}"\
+               f"@{os.getenv(POSTGRES_SERVER)}"\
+               f":{os.getenv(POSTGRES_PORT)}"\
+               f"/{os.getenv(POSTGRES_DB)}"
 # UPDATE ABOVE TO READ FROM Postgres
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
