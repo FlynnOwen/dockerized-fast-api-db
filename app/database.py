fb336c65af2e5ff69e7257ce267146b449afb00d
Ubuntu <ubuntu@ip-172-31-37-191.ap-southeast-2.compute.internal>
2022-04-29 08:50:35 +0000

    remove postgres env vars

--- app/database.py
+++ app/database.py
@@ -6,17 +6,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
 
 #DATABASE_URL = "sqlite:///./test.db"
-DATABASE_URL = f"postgresql://{os.getenv(POSTGRES_USER)}"\
-               f":{os.getenv(POSTGRES_PASSWORD)}"\
-               f"@{os.getenv(POSTGRES_SERVER)}"\
-               f":{os.getenv(POSTGRES_PORT)}"\
-               f"/{os.getenv(POSTGRES_DB)}"
+DATABASE_URL = f"postgresql://FlynnTest:FlynnTest123@db:5432/db"
 # UPDATE ABOVE TO READ FROM Postgres
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
